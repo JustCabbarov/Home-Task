811c6c8a944cbdfcc11d903a63ae0e4dd5ba5732
Əli Cabbarov <elicabbarov88@gmail.com>
2024-10-10 03:49:22 -0700

    Task Complated

---
+++ Home Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Home_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Ortalam = 98;
+            if (Ortalam < 65)
+            {
+                Console.WriteLine("Kesildin");
+            }
+            else if (Ortalam>=65 && Ortalam < 85)
+            {
+                Console.WriteLine("Diplom");
+
+            } 
+            else if (Ortalam>=85 && Ortalam < 95)
+            {
+                Console.WriteLine("Şeref Diplomu");
+            }
+            else
+            {
+                Console.WriteLine("Yuksek Şeref Diplomu");
+            }
+
+        }
+    }
+}
---
+++ Home Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.Design;
+
+namespace Home_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i =1; i <=100; i++)
+            {
+                 if (i % 15 == 0)
+                {
+                    Console.WriteLine("Bolunur " +i );
+                } 
+            }
+        }
+    }
+}
